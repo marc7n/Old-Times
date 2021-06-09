96162102fd088dbb246d84e0364ea5f8499e1c3d
Marcin <marcin.gier@wp.pl>
2021-06-09 13:26:29 +0200

    initialize project

---
+++ OldTimes/src/main/java/pl/coderslab/OldTimes/OldTimesApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.OldTimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldTimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldTimesApplication.class, args);
+	}
+
+}
